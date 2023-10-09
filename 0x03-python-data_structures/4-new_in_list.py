90abde85fa5bebea3364015f720adb30c97c2736
Alamy <mestafaalami11@gmail.com>
2023-10-09 23:23:27 +0100

    Add 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     newList = my_list[:]
-
     if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    newList[idx] = element
-
-    return newList
+        return newList
+    else:
+        newList[idx] = element
+        return newList
