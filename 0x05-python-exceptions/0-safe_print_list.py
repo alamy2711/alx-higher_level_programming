67b11432b5d2568b7c6fd00ccbb8af349d5fa5c5
Alamy <mestafaalami11@gmail.com>
2023-10-24 22:34:45 +0100

    Add 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             lenght += 1
-    except:
+    except IndexError:
         print("", end="")
 
     print("")
