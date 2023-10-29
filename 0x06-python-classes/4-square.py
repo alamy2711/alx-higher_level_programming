9152c2ad4b6711843bebf5f117f438782926cd35
Alamy <mestafaalami11@gmail.com>
2023-10-29 22:34:46 +0100

    Add 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,16 +15,8 @@ class Square:
 
         Args:
             size (int): The size of the square
-
-        Raises:
-            TypeError: If size is not an integer
-            ValueError: If size is < 0
         """
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
@@ -37,6 +29,7 @@ class Square:
 
         return self.__size**2
 
+    @property
     def size(self):
         """
         Returns the size of the square.
@@ -47,6 +40,7 @@ class Square:
 
         return self.__size
 
+    @size.setter
     def size(self, value):
         """
         Sets the size of the square.
