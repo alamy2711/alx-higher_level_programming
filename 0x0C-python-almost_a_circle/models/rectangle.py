270834fd2142fc7a7108aeae90b229c51bd50d9b
Alamy <mestafaalami11@gmail.com>
2023-11-13 23:11:10 +0100

    Add rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,11 +82,11 @@ class Rectangle(Base):
     # Display Method
     def display(self):
         """Display the rectangle"""
-        for k in range(self.__x):
+        for k in range(self.__y):
             print()
 
         for i in range(self.__height):
-            for k in range(self.__y):
+            for k in range(self.__x):
                 print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
