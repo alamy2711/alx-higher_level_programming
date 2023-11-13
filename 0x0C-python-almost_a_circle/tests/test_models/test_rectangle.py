aa8d541dba71bbb81dd91e01ed23a1a29fe5c0a7
Alamy <mestafaalami11@gmail.com>
2023-11-13 22:07:02 +0100

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,14 +5,18 @@ from io import StringIO
 from models.base import Base
 from models.rectangle import Rectangle
 
+"""Test Module"""
+
 
 class TestRectangle(unittest.TestCase):
     """Test the Rectangle class methods"""
 
     def setUp(self):
+        """Set up method for test cases"""
         Base._Base__nb_objects = 0
 
     def test_id(self):
+        """Test id assignment"""
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(10, 2, 0, 0, 12)
@@ -21,6 +25,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r3.id, 12)
 
     def test_raises(self):
+        """Test exception handling"""
         with self.assertRaises(TypeError):
             Rectangle(10, "2")
         with self.assertRaises(ValueError):
@@ -33,6 +38,7 @@ class TestRectangle(unittest.TestCase):
             Rectangle(10, 2, 3, -1)
 
     def test_area(self):
+        """Test area calculation"""
         r1 = Rectangle(3, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(8, 7, 0, 0, 12)
@@ -42,6 +48,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r3.area(), 56)
 
     def test_display(self):
+        """Test display method"""
         r1 = Rectangle(4, 6)
         r2 = Rectangle(2, 2)
         res1 = "####\n####\n####\n####\n####\n####\n"
