b9cf74421236049cb8e65c6fe9cf76717e3cf8c3
Alamy <mestafaalami11@gmail.com>
2023-12-14 03:01:19 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- Script that creates the database hbtn_0d_usa and the table states.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT,
 	name VARCHAR(256) NOT NULL
 );
