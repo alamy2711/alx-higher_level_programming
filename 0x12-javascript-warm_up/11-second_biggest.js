fce8b9af6aa06e29e285e830bcfa609fda3c5232
Alamy <mestafaalami11@gmail.com>
2024-01-09 00:58:50 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,22 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv.length === 2 || argv.length === 3) console.log(0);
-else {
-  let second = 0;
-  let first = argv[2];
-  for (let i = 2; i < argv.length; i++) {
-    if (first < argv[i]) {
-      if (second < first) second = first;
-      first = argv[i];
+if (argv.length <= 3) {
+    console.log(0);
+} else {
+    let first = Number(argv[2]);
+    let second = Number(argv[3]);
+
+    for (let i = 3; i < argv.length; i++) {
+        const currentNumber = Number(argv[i]);
+
+        if (currentNumber > first) {
+            second = first;
+            first = currentNumber;
+        } else if (currentNumber > second && currentNumber < first) {
+            second = currentNumber;
+        }
     }
-  }
-  console.log(second);
+
+    console.log(second);
 }
