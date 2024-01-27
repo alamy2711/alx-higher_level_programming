298fb7dd0c73875e63b447e39ecd719160df1c7f
Alamy <mestafaalami11@gmail.com>
2024-01-27 01:35:48 +0100

    Add 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: fetch_url.py
+
+Description:
+This script takes in a URL, sends a request to the URL,
+and displays the body of the response (decoded in utf-8).
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        # Send a request to the specified URL
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+
+    except error.HTTPError as err:
+        # Handle HTTP errors and display the error code
+        print('Error code:', err.code)
