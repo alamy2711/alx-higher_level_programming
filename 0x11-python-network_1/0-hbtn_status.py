d76e0ce214aa6df8861466b6e46bc7ebe6f33875
Alamy <mestafaalami11@gmail.com>
2024-01-27 01:21:17 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,13 +14,11 @@ def fetch_hbtn_status():
     """
     Fetches https://intranet.hbtn.io/status and prints response information.
     """
-    if __name__ == "__main__":
-        url = "https://intranet.hbtn.io/status"
-        with request.urlopen(url) as response:
-            content = response.read()
-            utf8_content = content.decode(encoding='utf-8')
 
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(utf8_content))
+    if __name__ == "__main__":
+        with request.urlopen("https://intranet.hbtn.io/status") as res:
+            res = res.read()
+            print("Body res:")
+            print("\t- type: {}".format(type(res)))
+            print("\t- content: {}".format(res))
+            print("\t- utf8 content: {}".format(res.decode(encoding='utf-8')))
