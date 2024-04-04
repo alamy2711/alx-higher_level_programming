77552db5494c667ee25473ac6930ce9e85a0d184
Alamy <mestafaalami11@gmail.com>
2024-04-04 02:54:17 +0000

    Add 103-script.js

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,20 @@
+/**
+ * This script fetches and prints how to say “Hello” depending on the language
+ */
+function makeTranslate() {
+    const url = 'https://www.fourtonfish.com/hellosalut/?';
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), (data) => {
+        $('DIV#hello').html(data.hello);
+    });
+}
+
+$('document').ready(() => {
+    $('INPUT#btn_translate').click(makeTranslate);
+    $('INPUT#language_code').focus(() => {
+        $(this).keydown((e) => {
+            if (e.keyCode === 13) {
+                makeTranslate();
+            }
+        });
+    });
+});
